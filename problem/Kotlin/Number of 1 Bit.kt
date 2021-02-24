a101b31a8667e4bc92deca6a1d9fb2c39c5bdc87
TayuHuang <404262470@gapp.fju.edu.tw>
2021-02-24 18:11:02 +0800

    Number of 1 bit kotlin best Ans

--- problem/Kotlin/Number of 1 Bit.kt
+++ problem/Kotlin/Number of 1 Bit.kt
@@ -11,3 +11,16 @@ class Solution {
         return x;
     }
 }
+
+/*
+class Solution {
+    // you need treat n as an unsigned value
+    fun hammingWeight(n:Int):Int = when {
+        n < 0 -> 1 + hammingWeight( n xor (1 shl 31))
+        n == 0 -> 0
+        n % 2 == 1 -> 1 + hammingWeight(n / 2)
+        else -> hammingWeight(n / 2)
+    }
+}
+the best and hard to understand code
+ */
