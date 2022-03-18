6609a0d9b0bd1d98bce995f2c2fde5e5474192e4
TaYu <404262470@gapp.fju.edu.tw>
2022-03-18 18:00:48 +0800

    Problem seven change to one line use kotlin

--- problem/bry/Kotlin/7.Reverse-Integer.kt
+++ problem/bry/Kotlin/7.Reverse-Integer.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun reverse(x: Int): Int {
-        var s: String = x.toString()
-        val ans  = try{if(x < 0) s.subSequence(1, s.length).reversed().toString().toInt().times(-1) else s.reversed().toInt() }catch(e: Exception){0}
-        return ans
-    }
+    fun reverse(x: Int): Int = try{if(x < 0) x.times(-1).reversed().times(-1) else x.reversed()}catch(e: Exception){0}
 }
